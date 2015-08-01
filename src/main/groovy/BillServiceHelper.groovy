04f10481a53a291b8961158fe53139cc50b5b428
Rob Clayforth <rclayforth@googlemail.com>
2015-08-01 10:27:29 +0100

    Fixed and being on same line as previous and

---
+++ src/main/groovy/BillServiceHelper.groovy
@@ -0,0 +1,5 @@
+/**
+ * Created by Robert Clayforth on 01/08/2015.
+ */
+class BillServiceHelper {
+}
---
+++ src/test/groovy/BillServiceHelperSpec.groovy
@@ -0,0 +1,7 @@
+import spock.lang.Specification
+
+/**
+ * Created by Robert Clayforth on 01/08/2015.
+ */
+class BillServiceHelperSpec extends Specification {
+}
